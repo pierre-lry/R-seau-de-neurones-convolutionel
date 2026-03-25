1eb441860ce98814c144fca40a95a7530f282ba3
p.l <pierre.lry31@gmail.com>
2026-03-25 08:49:17 +0100

    modif fonction padding + creation fonction dim_image

--- main.py
+++ main.py
@@ -10,24 +10,27 @@ class CNN:
         return np.where(x>0,x,0)
     def softmax(self,x):
         return np.exp(x) / np.sum(np.exp(x))
-    def padding(self,image,dimension_image,valeur_padding,h_padding,w_padding):
+    def dim_image(self, image): #fonction pour donner la dim de l'image sous la forme (H,W,D)
+        shape = image.shape
+        if len(shape) == 2 :
+            H,W = shape
+            D = 1
+        else :
+            H,W,D = shape
+        return H,W,D
+    def padding(self,image,valeur_padding,h_padding,w_padding):
         '''
         :param image --> matrice considérée,
-        dimension_image --> (H,W,D)
         valeur_padding --> la valeur des pixels qu'on ajoute,
         h_padding --> hauteur du padding
         w_padding --> largeur du padding
         :
         :return:
         '''
-        for i in range(dimension_image[2]):
-            nouvelle_image=np.full((h_padding+dimension_image[0],w_padding+dimension_image[1],dimension_image[2]),valeur_padding)
-
-        pass
-    def convolution(self,image,filtres,nb_filtres,taille_filtres,pas): '''
-    renvoie une liste de matrices de dimension qui dépend du pas et du padding. 
-    Les nb de matrices correspond aux nb de filtres (canaux)'''
-        return image
+        padded_image = np.pad(image, ((h_padding,h_padding), (w_padding,w_padding), (0,0)), mode='constant', constant_values=valeur_padding)
+        return padded_image
+    def convolution(self,image,filtres,nb_filtres,taille_filtres,pas): '''renvoie une liste de matrices de dimension qui dépend du pas et du padding. Les nb de matrices correspond aux nb de filtres (canaux)'''
+        #return image
     def maxPooling(self,image):
         pass
     def averagePooling(self):
@@ -38,4 +41,3 @@ class CNN:
         pass
     def backward(self):
         pass
-
